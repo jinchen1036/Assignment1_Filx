edbe247c575672aa498078f55600221cc0007a13
Shunling Chen <littleshunling@hotmail.com>
2018-09-22 22:46:24 -0400

    Initial Commit

---
+++ Assignment1_Filx/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Assignment1_Filx
+//
+//  Created by Jin Chen on 9/22/18.
+//  Copyright © 2018 Jin Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
